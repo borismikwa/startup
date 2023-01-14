a6faf4577b20361b1c30c3ab69c773e665273615
Mikwa Boris Tamanjong <borismikwa@yahoo.co.uk>
2023-01-14 18:35:56 +0100

    all service pages and their urls added

--- website/urls.py
+++ website/urls.py
@@ -15,5 +15,14 @@ urlpatterns = [
     path('demo/model/classification', views.classification_demo, name='classification_demo'),
     path('demo/model/regression',views.regression_demo,name='regression_demo'),
     path('products/', views.product, name='product'),
-    
+    path('services/collection/',views.data_collection,name='data_collection'),
+    path('services/analytics/',views.data_analytics, name='data_analytics'),
+    path('services/data_infrastructure/',views.data_infrastructure, name='data_infrastructure'),
+    path('services/machine_learning/',views.machine_learning,name='machine_learning'),
+    path('services/system_automatization/',views.system_automization,name='system_automization'),
+    path('services/process_optimization/',views.process_optimization,name='process_optimization'),
+    path('services/training_consultancy/', views.training_consultancy,name='training_consultancy'),
+    path('services/video_surveillance/',views.video_surveillance,name='video_surveillance'),
+    path('services/application_modernization/',views.application_modernization,name='application_modernization'),
+    path('services/software_dev/',views.software_dev,name='software_dev'),
 ]
