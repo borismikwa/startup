f3eba3c3a92fa333ed92cc398706a6d06d6a2cea
pius-ng3a <pius2016@gmail.com>
2023-01-17 07:51:07 +0100

    model update

--- website/models.py
+++ website/models.py
@@ -158,7 +158,8 @@ class Subscription(models.Model): #payments to consume a product or service
 class Service(models.Model): #services offered like the different programs
     service_name = models.CharField(max_length=150) 
     status = models.BooleanField(default=True) #active or not
-    description = models.CharField(max_length=255)
+    short_description = models.CharField(max_length=150)
+    description = models.TextField()
     date_included = models.DateTimeField() #when did we start offering the service
     price = models.CharField(max_length=30)
     currency = models.CharField(max_length=150) #needed for conversion purposes
@@ -167,7 +168,7 @@ class Service(models.Model): #services offered like the different programs
     created = models.DateTimeField(auto_now_add=True) # date created
     updated = models.DateTimeField(auto_now=True)#date updated
     promotion_end_date = models.DateTimeField(null=True)
-
+    avatar = models.ImageField(upload_to="img/services/")
     
     class Meta:
         verbose_name_plural ="Services"
