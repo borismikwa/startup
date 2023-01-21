2414bdbecc0b20a33b76234a1d82ba2aa78e11bd
Mikwa Boris Tamanjong <borismikwa@yahoo.co.uk>
2023-01-21 07:14:06 +0100

    avatar settings done

--- startup/settings.py
+++ startup/settings.py
@@ -14,8 +14,9 @@ from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+MEDIA_URL = '/media/'  
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
